8a2a5de634925614b833378ab4c7986b24fbe24f
Charles <carlosjosedesign@gmail.com>
2022-08-24 13:40:42 -0300

    quill editor

--- resources/views/admin/produtos/create.blade.php
+++ resources/views/admin/produtos/create.blade.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-file-uploader.css')) }}">
     <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
     <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-wizard.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-quill-editor.css')) }}">
     <link rel="stylesheet" href="{{ asset(mix('css/base/pages/page-produtos.css')) }}">
 @endsection
 
@@ -73,54 +74,14 @@
                                 <input type="text" class="form-control" name="tx_produto" placeholder="Nome" required />
                             </div>
 
-                            <div class="mb-1 col-12">
+                            <div class="mb-1 col-12 mb-5 pb-3">
                                 <label class="form-label">Descrição</label>
-                                <textarea class="form-control" name="tx_descricao" placeholder="Descrição..." required> </textarea>
-                                {{-- <div id="snow-wrapper">
-                                    <div id="snow-container">
-                                      <div class="quill-toolbar">
-                                        <span class="ql-formats">
-                                          <select class="ql-header">
-                                            <option value="1">Heading</option>
-                                            <option value="2">Subheading</option>
-                                            <option selected>Normal</option>
-                                          </select>
-                                          <select class="ql-font">
-                                            <option selected>Sailec Light</option>
-                                            <option value="sofia">Sofia Pro</option>
-                                            <option value="slabo">Slabo 27px</option>
-                                            <option value="roboto">Roboto Slab</option>
-                                            <option value="inconsolata">Inconsolata</option>
-                                            <option value="ubuntu">Ubuntu Mono</option>
-                                          </select>
-                                        </span>
-                                        <span class="ql-formats">
-                                          <button class="ql-bold"></button>
-                                          <button class="ql-italic"></button>
-                                          <button class="ql-underline"></button>
-                                        </span>
-                                        <span class="ql-formats">
-                                          <button class="ql-list" value="ordered"></button>
-                                          <button class="ql-list" value="bullet"></button>
-                                        </span>
-                                        <span class="ql-formats">
-                                          <button class="ql-link"></button>
-                                          <button class="ql-image"></button>
-                                          <button class="ql-video"></button>
-                                        </span>
-                                        <span class="ql-formats">
-                                          <button class="ql-formula"></button>
-                                          <button class="ql-code-block"></button>
-                                        </span>
-                                        <span class="ql-formats">
-                                          <button class="ql-clean"></button>
-                                        </span>
-                                      </div>
-                                      <div id="editor">
+                                <div id="editor">
+                                    <p>Hello World!</p>
+                                    <p>Some initial <strong>bold</strong> text</p>
+                                    <p><br></p>
+                                </div>
 
-                                      </div>
-                                    </div>
-                                  </div> --}}
                             </div>
 
                             <div class="col-md-6 ">
@@ -588,16 +549,37 @@
     <!-- Page js files -->
     <script src="{{ asset(mix('js/scripts/forms/form-select2.js')) }}"></script>
     <script src="{{ asset(mix('js/scripts/sortable/html5sortable.js')) }}"></script>
-    <script src="{{ asset(mix('js/scripts/forms/form-quill-editor.js')) }}"></script>
     <script src="{{ asset(mix('js/scripts/forms/form-images.js')) }}"></script>
 
 <script>
-    // var quill = new Quill('#editor', {
-    // modules: {
-    //     toolbar: '#toolbar'
-    // },
-    // theme: 'snow'
-    // });
+   var toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+        ['link','image','video'],
+
+        [{ 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+
+        [{ 'header': [2, 3, 4, 5, 6, false] }],
+
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+
+        ['clean']                                         // remove formatting button
+    ];
+    var quill = new Quill('#editor', {
+    modules: {
+        toolbar: toolbarOptions,
+        history: {          // Enable with custom configurations
+            'delay': 2500,
+            'userOnly': true
+        },
+    },
+    theme: 'snow'
+    });
 
 
 $('#tp_variacao').change(function() {
